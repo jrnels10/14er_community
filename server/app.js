568d432c190c54327f953a3d050a287d2b89ccb1
Jacob <jrnels10@gmail.com>
2019-08-08 17:16:22 -0700

    forgot to set to production

--- server/app.js
+++ server/app.js
@@ -3,14 +3,15 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 // const db = process.env.MONGODB_URI
 // console.log(process.env.NODE_ENV)
-const db = keys.state === 'production'? process.env.MONGODB_URI:keys.mongoDB.dbURI;
+const db = keys.state === 'production' ? process.env.MONGODB_URI : keys.mongoDB.dbURI;
 
 
 
-mongoose.connect(db,{ useNewUrlParser: true }, ()=>{
+mongoose.connect(db, { useNewUrlParser: true }, () => {
 
     console.log("connected to mongodb");
 });
@@ -29,10 +30,16 @@ app.use(bodyParser.json());
 
 
 //routes  http://localhost:3000/users
-app.use('/users',require('./routes/users'));
+app.use('/users', require('./routes/users'));
 
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 //start the server
 const port = process.env.PORT || 5000;
