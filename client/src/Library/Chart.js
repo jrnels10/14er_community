fe467295eb35fa7d15a0e00c504e90ee670431d1
Jacob <jrnels10@gmail.com>
2019-10-16 18:13:43 -0700

    adding to master

--- client/src/Library/Chart.js
+++ client/src/Library/Chart.js
@@ -3,21 +3,31 @@ import moment from 'moment';
 import { Chart } from 'chart.js';
 import './chart.css';
 
-async function dataLabels(myPeaksCompleted){
-    
-}
-async function dataTally(data) {
+
+async function dataTally(data, format) {
     let dateArray = data.map(item => {
         return item.dateCompleted
     })
     let convertedMonths = dateArray.map((item) => {
-        return moment(item).format('MMMM')
+        return moment(item).format(format)
     });
-    // convertedMonths.filter(function (item, pos) {
-    //     return convertedMonths.indexOf(item) === pos;
-    // });
     var counts = {};
     convertedMonths.map((item, idx) => {
+        counts[item] = counts[item] ? counts[item] + 1 : 1;
+        return null;
+    })
+    return counts;
+}
+
+async function overAllDataTally(data, format) {
+    let test = []
+    data.map(item => {
+        return item.attribute.completed.userCompleted.map(user => {
+            return test.push( moment(user.completedDate).format(format))
+        })
+    });
+    var counts = {};
+    test.map((item, idx) => {
         counts[item] = counts[item] ? counts[item] + 1 : 1;
         return null;
     })
@@ -30,30 +40,27 @@ export default class Graph extends Component {
         this.state = {};
     }
     async componentDidMount() {
-        console.log(this.props)
         const { user: { myPeaksCompleted } } = this.props.data;
-        console.log(myPeaksCompleted)
-
-        let newData = await dataTally(myPeaksCompleted)
-        console.log(newData)
-        var ctx = document.getElementById('myChart').getContext('2d');
+        // let overAllData = await overAllDataTally(this.props.data.peaks.allPeaksCompleted, this.props.type)
+        let newData = await dataTally(myPeaksCompleted, this.props.type)
+        var ctx = document.getElementById(`myChart-${this.props.type}`).getContext('2d');
         new Chart(ctx, {
-            type: 'doughnut',
+            type: 'line',
             data: {
                 labels: Object.keys(newData),
                 datasets: [{
-                    label: '# of Votes',
+                    label: 'Me',
                     data: Object.values(newData),
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(162, 12, 41, 0.65)',
+                        'rgba(54, 162, 235, 0.65)',
+                        'rgba(255, 206, 86, 0.65)',
+                        'rgba(75, 192, 192, 0.65)',
+                        'rgba(153, 102, 255, 0.65)',
+                        'rgba(255, 159, 64, 0.65)'
                     ],
                     borderColor: [
-                        'rgba(255, 99, 132, 1)',
+                        'rgba(162, 12, 41, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
@@ -61,24 +68,27 @@ export default class Graph extends Component {
                         'rgba(255, 159, 64, 1)'
                     ],
                     borderWidth: 1
-                }]
+                }
+            ]
             },
-            // options: {
-            //     scales: {
-            //         yAxes: [{
-            //             ticks: {
-            //                 beginAtZero: true
-            //             }
-            //         }]
-            //     }
-            // }
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
         })
     }
 
 
     render() {
         return (<div id="chart-container">
-            <canvas id="myChart" width="400" height="400"></canvas>
+            {this.props.children}
+            <hr />
+            <canvas id={`myChart-${this.props.type}`} width="400" height="400"></canvas>
         </div>
         );
     }
--- client/src/components/UserProfile/UserDetails.js
+++ client/src/components/UserProfile/UserDetails.js
@@ -10,9 +10,10 @@ export default class UserDetails extends Component {
     render() {
         return this.props.value.user.email !== '' ? (
             <div id="user-details" className={`details-${this.props.show}`}>
-                <h4>Completed occurence</h4>
-                <hr />
-                <Chart data={this.props.value} />
+                <h4>Most populary by month</h4>
+                
+                <Chart data={this.props.value} type="MMMM"/>
+                <Chart data={this.props.value} type="YYYY"/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         ) : null;
